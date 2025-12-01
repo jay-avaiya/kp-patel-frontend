5c5157b0e08e53223c62ef0cc55bdd70c6079fd0
Vinit Menon <vp.menon06@gmail.com>
2025-12-01 10:12:21 +0530

    coookies

--- src/layouts/main_component/Cookies.jsx
+++ src/layouts/main_component/Cookies.jsx
@@ -10,20 +10,20 @@ const Cookies = () => {
   }, []);
 
   const acceptCookie = () => {
-    localStorage.setItem("cookieConsent", true);
+    localStorage.setItem("cookieConsent", "accepted");
     setOpen(false);
   };
 
   const rejectCookie = () => {
-    localStorage.setItem("cookieConsent", false);
+    localStorage.setItem("cookieConsent", "rejected");
     setOpen(false);
   };
 
   if (!open) return null;
 
   return (
-    <div className="fixed bottom-0 left-0 z-50 py-6 px-11 bg-white flex flex-row gap-24 w-full items-center justify-center rounded-[20px]">
-      <div className="w-[200px] h-[250px]">
+    <div className="fixed bottom-0 left-0 z-60 py-3 lg:py-6 px-11 bg-white flex flex-row gap-24 w-full items-center justify-center rounded-t-[20px]">
+      <div className="hidden md:flex md:w-[100px] md:h-[150px] lg:w-[130px] lg:h-[180px] xl:w-[200px] xl:h-[250px]">
         <img
           src="/cookie.png"
           alt="cookie"
@@ -31,9 +31,11 @@ const Cookies = () => {
         />
       </div>
 
-      <div className="font-lato  leading-[160%] w-[40%] flex flex-col gap-6">
-        <h3 className="text-3xl font-extrabold">We Use Cookies</h3>
-        <p className="text-xl font-normal">
+      <div className="font-lato  leading-[160%] lg:w-[60%] xl:w-[40%] flex flex-col gap-6">
+        <h3 className="text-base md:text-lg lg:text-2xl xl:text-3xl font-extrabold">
+          We Use Cookies
+        </h3>
+        <p className="text-sm md:text-[16px] lg:text-lg xl:text-xl font-normal">
           This website uses cookies to improve your experience. By clicking
           “Accept” you are agreeing to our
           <Link className="text-[#19C2FB] underline"> Cookie Policy</Link>.
@@ -42,13 +44,13 @@ const Cookies = () => {
         <div className="flex flex-row gap-5">
           <button
             onClick={acceptCookie}
-            className="rounded-full w-[50%] bg-[#F94223] text-white font-medium cursor-pointer text-2xl"
+            className="rounded-full w-[50%] bg-[#F94223] text-white font-medium cursor-pointer text-sm md:text-[16px] lg:text-xl xl:text-2xl"
           >
             Accept
           </button>
           <button
             onClick={rejectCookie}
-            className="rounded-full w-[50%]  text-[#F94223] border border-[#F94223] font-medium cursor-pointer text-2xl"
+            className="rounded-full w-[50%]  text-[#F94223] border border-[#F94223] font-medium cursor-pointer text-sm md:text-[16px] lg:text-xl xl:text-2xl"
           >
             Reject All
           </button>
